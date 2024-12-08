df8d810387794a9a180b8a898e33a175e06305cb
Damjan15 <stamenovdamjan5@gmail.com>
2024-12-08 01:10:36 +0100

    feat: updated Car model to include all relationships

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -32,28 +32,53 @@ class Car extends Model
         'published_at',
     ];
 
-    public function features(): HasOne
-    {
-        return $this->hasOne(CarFeature::class, 'car_id');
-    }
-
-    public function primaryImage(): HasOne
-    {
-        return $this->hasOne(CarImage::class)->ofMany('position', 'min');
-    }
-
-    public function images(): HasMany
-    {
-        return $this->hasMany(CarImage::class,);
-    }
-
     public function carType(): BelongsTo
     {
         return $this->belongsTo(CarType::class);
     }
 
+    public function fuelType(): BelongsTo
+    {
+        return $this->belongsTo(FuelType::class);
+    }
+
+    public function maker(): BelongsTo
+    {
+        return $this->belongsTo(Maker::class);
+    }
+
+    public function model(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\Model::class);
+    }
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function features(): HasOne
+    {
+        return $this->hasOne(CarFeature::class);
+    }
+
+    public function primaryImage(): HasOne
+    {
+        return $this->hasOne(CarImage::class)->orderBy('position')->limit(1);
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(CarImage::class);
+    }
+
     public function favouredUsers(): BelongsToMany
     {
-        return $this->belongsToMany(User::class, 'favourite_cars', 'car_id', 'user_id');
+        return $this->belongsToMany(User::class, 'favourite_cars');
     }
 }
